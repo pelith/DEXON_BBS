32023a4c824deac2294b11d8d00c607e5d15f860
artistic709 <artistic709@gmail.com>
2019-05-07 22:53:51 +0800

    initial commit

---
+++ abi.js
@@ -0,0 +1,17 @@
+var ABI = [
+    {
+        "constant": false,
+        "inputs": [{ "name": "content", "type": "string" }],
+        "name": "Post",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [{ "indexed": false, "name": "content", "type": "string" }],
+        "name": "Posted",
+        "type": "event"
+    }
+]
