c725469258f5f2abb8b87d91aeed675fe957dab8
DEXON <deploy@dett.cc>
2019-05-09 19:21:22 +0000

    Automated deployment to GitHub Pages: 7f100c5b148f8675cf3ab3907dea6ddce36fe325

--- post.974de036.js
+++ post.974de036.js
@@ -405,14 +405,77 @@ var _utils = require("./utils.js");
 
 var _dexon = require("./dexon.js");
 
+var checkContent = function checkContent() {
+  return $("#bbs-content")[0].value.length > 0;
+};
+
+var checkTitle = function checkTitle() {
+  return $("#bbs-title")[0].value > 0;
+};
+
+var check = function check() {
+  return checkContent() || checkTitle();
+};
+
 var activeDexonRender = function activeDexonRender(account) {
-  $("#bbs-post")[0].disabled = $("#bbs-content")[0].value.length > 0 && $("#bbs-title")[0].value > 0 ? false : true;
+  $("#bbs-post")[0].disabled = !check();
   $("#bbs-user")[0].innerHTML = (0, _utils.getUser)(account);
+};
+
+var keyboardHook = function keyboardHook() {
+  var ctrlKey = 17,
+      cmdKey = 91,
+      QKey = 81,
+      XKey = 88,
+      YKey = 89;
+  var ctrlDown = false;
+  var checkSave = false,
+      checkPost = false;
+  $(document).keydown(function (e) {
+    if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+  }).keyup(function (e) {
+    if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+  });
+  $(document).keydown(function (e) {
+    if (ctrlDown && e.keyCode == QKey) {
+      $("#bbs-footer")[0].style.display = 'none';
+      $("#bbs-checksave")[0].style.display = '';
+      $("#bbs-title")[0].disabled = true;
+      $("#bbs-content")[0].disabled = true;
+      checkSave = true; // window.location = 'index.html'
+    } else if (ctrlDown && e.keyCode == XKey) {
+      if (check()) {
+        $("#bbs-footer")[0].style.display = 'none';
+        $("#bbs-checkpost")[0].style.display = '';
+        $("#bbs-title")[0].disabled = true;
+        $("#bbs-content")[0].disabled = true;
+        checkPost = true;
+      }
+    } else if (48 <= e.keyCode && e.keyCode <= 222) {
+      if (checkSave) {
+        $("#bbs-footer")[0].style.display = '';
+        $("#bbs-checksave")[0].style.display = 'none';
+        $("#bbs-title")[0].disabled = false;
+        $("#bbs-content")[0].disabled = false;
+        checkSave = false;
+        if (e.keyCode == YKey) window.location = 'index.html';
+      } else if (checkPost) {
+        $("#bbs-footer")[0].style.display = '';
+        $("#bbs-checkpost")[0].style.display = 'none';
+        $("#bbs-title")[0].disabled = false;
+        $("#bbs-content")[0].disabled = false;
+        checkPost = false;
+        if (e.keyCode == YKey) if (check()) (0, _dexon.newPost)($("#bbs-title")[0].value, $("#bbs-content")[0].value);
+      }
+    }
+  });
 };
 
 function main() {
   // String.prototype.lines = function() { return this.split(/\r*\n/); }
   // String.prototype.lineCount = function() { return this.lines().length; }
+  keyboardHook();
+
   $("#bbs-title")[0].onblur = function () {
     $("#bbs-title")[0].value = (0, _utils.getParseText)($("#bbs-title")[0].value, 40);
   };
@@ -422,14 +485,15 @@ function main() {
   $("#bbs-content")[0].placeholder = "~\n".repeat(20);
 
   $("#bbs-post")[0].onclick = function () {
-    (0, _dexon.newPost)($("#bbs-title")[0].value, $("#bbs-content")[0].value);
+    if (!checkContent() && !checkTitle() || confirm('確定發文?')) (0, _dexon.newPost)($("#bbs-title")[0].value, $("#bbs-content")[0].value);
   };
 
   $("#bbs-cancel")[0].onclick = function () {
-    window.location = 'index.html';
+    if (!checkContent() && !checkTitle() || confirm('結束但不儲存?')) window.location = 'index.html';
   };
 
   (0, _dexon.initDexon)(activeDexonRender);
+  ' ◆ 結束但不儲存 [y/N]?                                        ';
 }
 
 $(main());
