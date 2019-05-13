252295c8ce6825d0ab4f71de188c4994ea975d3a
sirius <sirius.tsou@gmail.com>
2019-05-13 21:44:18 +0800

    fix about page bugs

--- src/about.js
+++ src/about.js
@@ -3,39 +3,27 @@ import {initDexon, loginDexon} from './dexon.js'
 
 const activeDexonRender = (account) => {
   account = getUser(account)
-
+  
   if (account){
     // show User 
     $("#bbs-login").hide()
     $("#bbs-register").hide()
     $("#bbs-user").show()
-
-    // only show reply btn at first time 
-    if (!$("#reply-user").text()) $("#reply-btn").show()
   }
   else{
     // show Login/Register
     $("#bbs-login").show()
     $("#bbs-register").show()
     $("#bbs-user").hide()
-
-    // hide reply btn
-    $("#reply-btn").hide()
   }
   
   $("#bbs-user").text(account)
-  $("#reply-user").text(account)
 }
 
 
 const main = async () => {
   initDexon(activeDexonRender)
-
   $('#bbs-login').click(() => { loginDexon(activeDexonRender) })
-
-  $("#bbs-login").hide()
-  $("#bbs-register").hide()
-  $("#bbs-user").show()
 }
 
 $(main)
