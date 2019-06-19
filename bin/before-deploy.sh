ce478c21ea73d3720128304cd47b41b2056f4c6f
sirius <sirius.tsou@gmail.com>
2019-06-19 15:46:11 +0800

    update before-deploy.sh

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -3,6 +3,8 @@
 echo dett.cc > build/CNAME
 cp -r .circleci build
 cp src/assets/favicon.ico build
+cp src/assets/robots.txt build
+cp src/assets/sitemap.xml build
 
 mkdir -p build/assets/
 cp src/assets/meta.png build/assets
